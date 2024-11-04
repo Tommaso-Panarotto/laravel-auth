549b87b713daeab74cb03acd27426658e67d2bd3
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-04 17:14:46 +0100

    pages element

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.app")
+
+@section("content")
+ <h1>
+    Create Projects
+ </h1>
+@endsection
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+ <h1>
+    Index Projects
+ </h1>
+@endsection
