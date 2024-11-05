85bf64cdefe60498bb9a46c0a337d46c317d9fd5
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-05 16:24:52 +0100

    page index styled

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
- <h1>
-    Index Projects
- </h1>
+<div class="container text-center">
+    <h2>Progetti</h2>
+    <a href="{{route("admin.projects.create")}}" class="btn btn-primary mt-5">aggiungi un nuovo progetto</a>
+    <div class="row g-3 mt-5">
+        <table class="table table-striped-columns">
+
+        <thead>
+            <tr>
+                <th scope="col"> </th>
+                <th scope="col">Title</th>
+                <th scope="col">Language</th>
+                <th scope="col">url</th>
+            </tr>
+        </thead>
+
+         <tbody>
+          @forelse ($projects as $project )
+          <tr>
+            <th scope="row">{{ $project->id }}</th>
+            <td>{{ $project->title }}</td>
+          </tr>
+          @empty
+          <tr>
+             <h2>Non ci sono piante in catalogo....</h2>
+          </tr>
+        @endforelse
+        </tbody>
+
+        </table>
+    </div>
+    </div>
 @endsection
