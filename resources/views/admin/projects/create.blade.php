379b2988ee62bb6b5db201f47182fdc8cca976df
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-05 16:24:39 +0100

    page create form

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -1,7 +1,36 @@
 @extends("layouts.app")
 
 @section("content")
- <h1>
-    Create Projects
- </h1>
+<div class="container text-center">
+    <h1 class="mt-5">Aggiungi un progetto</h1>
+
+    <form class="mt-5" method="POST" action="{{route("admin.projects.store")}}">
+      @csrf
+
+      <div class="mb-3">
+        <label for="title" class="form-label">titolo prgetto:</label>
+        <input type="text" class="form-control" id="project-title" name="title" value="{{ old('title')}}">
+      </div>
+      <div class="mb-3">
+          <label for="author" class="form-label">nome autore:</label>
+          <input type="text" class="form-control" id="project-author" name="author" value="{{ old('author')}}">
+        </div>
+        <div class="mb-3">
+          <label for="language" class="form-label">linguaggio usato:</label>
+          <input type="text" class="form-control" id="project-language" name="language" value="{{ old('language')}}">
+        </div>
+        <div class="mb-3">
+          <label for="url" class="form-label">link progetto:</label>
+          <input type="text" class="form-control" id="project-url" name="url" value="{{ old('url')}}">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">descrizione progetto:</label>
+            <textarea class="form-control" id="project-description" rows="3" name="description">{{ old('description')}}</textarea>
+        </div>
+        <div class="buttons">
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="reset" class="btn btn-warning">Reset campi</button>
+        </div>
+    </form>
+  </div>
 @endsection
