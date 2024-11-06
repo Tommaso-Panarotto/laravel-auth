0842af27f1e5baa3352d83552d4d25b9613884f2
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-06 16:43:01 +0100

    route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Guest\ProjectController as GuestProjectController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -23,12 +24,20 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function(){
-Route::get("/projects", [AdminProjectController::class, "index"])->name("projects.index");
-Route::post("/projects", [AdminProjectController::class, "store"])->name("projects.store");
-Route::get("/projects/create", [AdminProjectController::class, "create"])->name("projects.create");
-Route::get("/projects/{id}", [AdminProjectController::class, "show"])->name("projects.show");
-Route::get("/projects/{id}/edit", [AdminProjectController::class, "edit"])->name("projects.edit");
-Route::put("/projects/{id}", [AdminProjectController::class, "update"])->name("projects.update");
-Route::delete("/projects/{id}", [AdminProjectController::class, "destroy"])->name("projects.delete");
+Route::prefix("/guest")->name("guest.")->group(function () {
+    Route::get("/projects", [GuestProjectController::class, "index"])->name("projects.index");
+    Route::get("/projects/{id}", [GuestProjectController::class, "show"])->name("projects.show");
+});
+
+Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function () {
+    Route::get("/projects", [AdminProjectController::class, "index"])->name("projects.index");
+    Route::get("/projects/deleted", [AdminProjectController::class, "deletedIndex"])->name("projects.deleted-index");
+    Route::post("/projects", [AdminProjectController::class, "store"])->name("projects.store");
+    Route::get("/projects/create", [AdminProjectController::class, "create"])->name("projects.create");
+    Route::get("/projects/{id}", [AdminProjectController::class, "show"])->name("projects.show");
+    Route::put("/projects/{id}", [AdminProjectController::class, "update"])->name("projects.update");
+    Route::delete("/projects/{id}", [AdminProjectController::class, "destroy"])->name("projects.delete");
+    Route::delete("/projects/{id}/permanent-delete", [AdminProjectController::class, "permanentDelete"])->name("projects.permament-delete");
+    Route::get("/projects/{id}/edit", [AdminProjectController::class, "edit"])->name("projects.edit");
+    Route::patch("/projects/{id}/restore", [AdminProjectController::class, "restore"])->name("projects.restore");
 });
