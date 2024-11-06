64d92a97bb27bfa5b2c99386a13813e3952f96c4
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-06 08:08:04 +0100

    added error validation to create and edit

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -10,22 +10,47 @@
       <div class="mb-3">
         <label for="title" class="form-label">titolo prgetto:</label>
         <input type="text" class="form-control" id="project-title" name="title" value="{{ old('title')}}">
+        @error('title')
+        <div class="alert alert-warning">
+          {{$message}}
+          </div>
+          @enderror
       </div>
       <div class="mb-3">
           <label for="author" class="form-label">nome autore:</label>
           <input type="text" class="form-control" id="project-author" name="author" value="{{ old('author')}}">
+          @error('author')
+          <div class="alert alert-warning">
+            {{$message}}
+            </div>
+            @enderror
         </div>
         <div class="mb-3">
           <label for="language" class="form-label">linguaggio usato:</label>
           <input type="text" class="form-control" id="project-language" name="language" value="{{ old('language')}}">
+          @error('language')
+          <div class="alert alert-warning">
+            {{$message}}
+            </div>
+            @enderror
         </div>
         <div class="mb-3">
           <label for="url" class="form-label">link progetto:</label>
           <input type="text" class="form-control" id="project-url" name="url" value="{{ old('url')}}">
+          @error('url')
+          <div class="alert alert-warning">
+            {{$message}}
+            </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">descrizione progetto:</label>
             <textarea class="form-control" id="project-description" rows="3" name="description">{{ old('description')}}</textarea>
+            @error('description')
+            <div class="alert alert-warning">
+              {{$message}}
+              </div>
+              @enderror
         </div>
         <div class="buttons">
             <button type="submit" class="btn btn-primary">Invia</button>
